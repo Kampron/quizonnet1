3eb4513389dd5b0ea6ebf28a28aff02256729a68
kampron <qwequkyei@gmail.com>
2023-06-08 06:10:55 +0000

    server

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://quizonnet-server.onrender.com",
+        target: "https://quizonnet-server.onrender.com/",
         changeOrigin: true,
         // secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
